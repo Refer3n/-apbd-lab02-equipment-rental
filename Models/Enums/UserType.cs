b3743bb211cad164c31586d3fbd88c5869b8dad9
Oleksandr Lavrynenko <s32656@pjwstk.edu.pl>
2026-03-17 13:01:57 +0100

    Add equipment and user enums

---
+++ Models/Enums/UserType.cs
@@ -0,0 +1,7 @@
+﻿namespace EquipmentRental.Models.Enums;
+
+public enum UserType
+{
+    Student,
+    Employee
+}
---
+++ Models/Equipment.cs
@@ -0,0 +1,22 @@
+﻿using EquipmentRental.Models.Enums;
+
+namespace EquipmentRental.Models;
+
+public abstract class Equipment
+{
+    public int Id { get; }
+    public string Name { get; set;  }
+    public EquipmentStatus Status { get; private set;  }
+
+    public Equipment(int id, string name)
+    {
+        Id = id;
+        Name = name;
+        Status = EquipmentStatus.Available;
+    }
+    
+    public void MarkAvailable() => Status = EquipmentStatus.Available;
+    public void MarkUnavailable() => Status = EquipmentStatus.Unavailable;
+    
+    public bool IsAvailable() => Status == EquipmentStatus.Available;
+}
