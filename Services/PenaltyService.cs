0940272bcdd974a5950f37eb46242e393b92aace
Alex <lerfiklavrik@gmail.com>
2026-03-22 00:36:05 +0100

    Add Penalty And Policy Service

---
+++ Services/PenaltyService.cs
@@ -0,0 +1,19 @@
+﻿namespace EquipmentRental.Services
+{
+    public class PenaltyService
+    {
+        private const decimal PenaltyPerDay = 5m;
+
+        public decimal CalculatePenalty(DateTime dueDate, DateTime returnDate)
+        {
+            int lateDays = (returnDate - dueDate).Days;
+
+            if(lateDays <= 0m)
+            {
+                return 0;
+            }
+
+            return lateDays * PenaltyPerDay;
+        }
+    }
+}
---
+++ Services/PolicyService.cs
@@ -0,0 +1,17 @@
+﻿using EquipmentRental.Models;
+
+namespace EquipmentRental.Services
+{
+    public class PolicyService
+    {
+        public int GetRentalLimit(User user)
+        {
+            return user switch
+            {
+                Student => 2,
+                Employee => 5,
+                _ => 0
+            };
+        }
+    }
+}
