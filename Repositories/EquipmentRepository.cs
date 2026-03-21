0b744ee6217e9928af1d3dff150d6c2585f68999
Alex <lerfiklavrik@gmail.com>
2026-03-22 00:16:00 +0100

    Add EquipmentRepository

---
+++ Repositories/EquipmentRepository.cs
@@ -0,0 +1,24 @@
+﻿using EquipmentRental.Models;
+
+namespace EquipmentRental.Repositories
+{
+    public class EquipmentRepository
+    {
+        private readonly List<Equipment> _equipmentItems = new();
+
+        public void Add(Equipment equipment)
+        {
+            _equipmentItems.Add(equipment);
+        }
+
+        public List<Equipment> GetAll()
+        {
+            return _equipmentItems;
+        }
+
+        public Equipment? GetById(int id)
+        {
+            return _equipmentItems.FirstOrDefault(e => e.Id == id);
+        }
+    }
+}
