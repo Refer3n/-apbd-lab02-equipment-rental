aff5c0c18526e1a0f833bbb8e60d11ec4e4af4c6
Alex <lerfiklavrik@gmail.com>
2026-03-22 00:09:58 +0100

    Add UserRepository

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using EquipmentRental.Models;
+
+namespace EquipmentRental.Repositories
+{
+    public class UserRepository
+    {
+        private readonly List<User> users = new();
+
+        public void Add(User user)
+        {
+            users.Add(user);
+        }
+
+        public List<User> GetAll()
+        {
+            return users;
+        }
+
+        public User? FindById(int id)
+        {
+            return users.FirstOrDefault(u => u.Id == id);
+        }
+    }
+}
