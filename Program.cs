eff370baa73ff097dbd9d7538cb6051f0c6ea549
Alex <lerfiklavrik@gmail.com>
2026-03-22 01:23:32 +0100

    Add application startup in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using EquipmentRental.Repositories;
+using EquipmentRental.Services;
+using EquipmentRental.Ui;
 
-Console.WriteLine("Hello, World!");
+var userRepository = new UserRepository();
+var equipmentRepository = new EquipmentRepository();
+var rentalRepository = new RentalRepository();
+
+var policyService = new PolicyService();
+var penaltyService = new PenaltyService();
+
+var userService = new UserService(userRepository);
+var equipmentService = new EquipmentService(equipmentRepository);
+var rentalService = new RentalService(
+    userRepository,
+    equipmentRepository,
+    rentalRepository,
+    policyService,
+    penaltyService);
+
+var reportService = new ReportService(
+    userRepository,
+    equipmentRepository,
+    rentalRepository);
+
+var menu = new ConsoleMenu(
+    userService,
+    equipmentService,
+    rentalService,
+    reportService);
+
+menu.RunDemonstrationScenario();
+
+//menu.Run();
